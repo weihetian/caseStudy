d8806c2c0bf5eec00775ab9db3ba10dd511c9198
weihetian <weihe@udel.edu>
2016-01-07 14:34:57 -0500

    single case

---
+++ js/_case.js
@@ -0,0 +1,19 @@
+$(function(){
+  var getUrlParameter = function getUrlParameter(sParam) {
+    var sPageURL = decodeURIComponent(window.location.search.substring(1)),
+        sURLVariables = sPageURL.split('&'),
+        sParameterName,
+        i;
+
+    for (i = 0; i < sURLVariables.length; i++) {
+        sParameterName = sURLVariables[i].split('=');
+
+        if (sParameterName[0] === sParam) {
+            return sParameterName[1] === undefined ? true : sParameterName[1];
+        }
+    }
+};
+  var querystring = window.location.querystring;
+  var caseId = getUrlParameter('case_id');
+  var caseName = getUrlParameter("case_title");
+});
