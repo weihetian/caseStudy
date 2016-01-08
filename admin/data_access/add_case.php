ba28e654cee736d1d207cf01c3f880cbb62e2182
weihetian <weihe@udel.edu>
2016-01-08 11:58:13 -0500

    casestudy backend data access

---
+++ admin/data_access/add_case.php
@@ -0,0 +1,30 @@
+<?php
+$servername = "52.23.43.247";
+$username = "stak";
+$password = "stakstudios";
+$database = "casestudy";
+// Create connection
+$conn = mysqli_connect($servername, $username, $password,$database,"3306");
+
+// Check connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+
+// TO DO
+$name = $_POST['name'];
+$description = $_POST['description'];
+
+$query = "INSERT INTO casestudy( ) VALUES( )";
+
+if (mysqli_query($conn, $query)) {
+    echo "succeed";
+} else {
+    echo "Error updating record: " . mysqli_error($conn);
+}
+
+mysqli_close($conn);
+
+
+
+?>
